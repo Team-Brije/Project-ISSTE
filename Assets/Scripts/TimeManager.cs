3e415de5ea0e46f863600f3e84f33f67fe19ea53
warcilac9 <114256884+warcilac9@users.noreply.github.com>
2024-09-09 16:08:54 -0600

    Habemos concepto de Day

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TimeManager : MonoBehaviour
 {
+    public int timer;
+    public TextMeshProUGUI Temporizador;
     // Start is called before the first frame update
     void Start()
     {
+        Invoke(nameof(TimerCountdown), 0);
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TimerCountdown()
     {
-        
+        timer--;
+        Temporizador.text = timer.ToString();
+
+        if (timer != 0)
+        {
+            Invoke(nameof(TimerCountdown), 1);
+        }
+        else
+        {
+            Debug.Log("Se acabo el dia");
+        }
     }
 }
