7a0fadf1ef85f0f33d39701c147e6d5437eb28e9
amauryxd <cdmx2472@amerike.edu.mx>
2024-09-18 09:59:45 -0600

    l

--- Assets/Scenes/graybox decal/raycastStamp.cs
+++ Assets/Scenes/graybox decal/raycastStamp.cs
@@ -17,7 +17,22 @@ public class raycastStamp : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       if(Physics.Raycast(transform.position,transform.TransformDirection(Vector3.forward), out RaycastHit hitinfo, distanceRay))
+        /*
+        if(Physics.Raycast(transform.position,transform.TransformDirection(Vector3.forward), out RaycastHit hitinfo, distanceRay))
+        {
+            Debug.Log("hay: "+hitinfo);
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hitinfo.distance, Color.red);
+            decalxd.transform.position = hitinfo.point;
+            decalxd.transform.rotation = selloxd.transform.rotation;
+        }
+        else
+        {
+            Debug.Log("no hay na: " + hitinfo.transform); Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hitinfo.distance, Color.blue);
+        }*/
+    }
+
+    public void activarSello(){
+        if(Physics.Raycast(transform.position,transform.TransformDirection(Vector3.forward), out RaycastHit hitinfo, distanceRay))
         {
             Debug.Log("hay: "+hitinfo);
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hitinfo.distance, Color.red);
