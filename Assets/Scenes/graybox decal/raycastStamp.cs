cb1986ddfbe49d5239eccbefb1a9fde59532c33e
EdsonGP723 <cdmx2472@amerike.edu.mx>
2024-09-18 17:46:49 -0600

    u

--- Assets/Scenes/graybox decal/raycastStamp.cs
+++ Assets/Scenes/graybox decal/raycastStamp.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class raycastStamp : MonoBehaviour
 {
+    public static event Action<bool> OnInteract;
+
     public GameObject decalxd;
     public float distanceRay;
     public GameObject selloxd;
@@ -38,10 +40,17 @@ public class raycastStamp : MonoBehaviour
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hitinfo.distance, Color.red);
             decalxd.transform.position = hitinfo.point;
             decalxd.transform.rotation = selloxd.transform.rotation;
+            //hitinfo.transform.gameObject.GetComponent<Ticket>().isTicketCorrrect;
+            //if (hitinfo.transform.gameObject.CompareTag("")) ;
+            Debug.Log(hitinfo.transform.gameObject.GetComponent<Ticket>().isTicketCorrrect);
+            bool ticketstatus = hitinfo.transform.gameObject.GetComponent<Ticket>().isTicketCorrrect;
+            OnInteract?.Invoke(ticketstatus);
         }
         else
         {
             Debug.Log("no hay na: " + hitinfo.transform); Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hitinfo.distance, Color.blue);
         }
+
+        
     }
 }
