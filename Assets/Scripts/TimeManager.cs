f533b0f5c32146bfe7ba9a3e34e72497549e9fc5
Shikatastrophe <warcilac9@gmail.com>
2024-09-24 15:36:01 -0600

    Day system

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -4,17 +4,35 @@ using System.Diagnostics.Tracing;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TimeManager : MonoBehaviour
 {
-    public int Day = 1;
-    public int timer;
+    //Variables for Day system
+    public static int Day = 1;
+    private int timer;
+    private static   int begTime = 5 ;
+    private int extraTime = 5;
+
+    //Variables For Ui
     public TextMeshProUGUI Temporizador;
     public GameObject boton;
+    public GameObject PlayButton;
+    
     // Start is called before the first frame update
     void Start()
     {
-        Invoke(nameof(TimerCountdown), 0);
+        if (Day == 1)
+        {
+
+            timer = begTime;
+        }else
+        {
+            begTime = begTime + extraTime;
+            timer = begTime;
+        }
+        
+        Debug.Log(begTime);
         
     }
 
@@ -23,6 +41,8 @@ public class TimeManager : MonoBehaviour
         timer--;
         Temporizador.text = timer.ToString();
 
+        PlayButton.SetActive(false);
+
         if (timer != 0)
         {
             Invoke(nameof(TimerCountdown), 1);
@@ -30,15 +50,24 @@ public class TimeManager : MonoBehaviour
         else
         {
             Debug.Log("Se acabo el dia");
-            timer = timer + 30;
+            //timer = timer + extraTime;
             boton.SetActive(true);
+            Day++;
+            //SceneManager.LoadScene("Timer");
+
         }
 
-        
+
     }
     public void Regresar()
     {
-        Invoke(nameof(TimerCountdown), 0);
+        //Invoke(nameof(TimerCountdown), 0);
         boton.SetActive(false);
+        SceneManager.LoadScene("Timer");
+    }
+
+    public void Play()
+    {
+        TimerCountdown();
     }
 }
