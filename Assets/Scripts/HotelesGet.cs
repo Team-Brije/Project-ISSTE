298c0fe6059c43257c2c0cdaeff3cdc71cf936ad
amauryxd <cdmx2472@amerike.edu.mx>
2024-11-08 15:30:07 -0600

    movi cosas no recuerdo q :bee:

--- Assets/Scripts/HotelesGet.cs
+++ Assets/Scripts/HotelesGet.cs
@@ -15,12 +15,12 @@ public class HotelesGet : MonoBehaviour
             if (managerTime.CurDay.Hotel1) { currentText.text += "<br>Hotel 1 Avialiable"; }
             if (managerTime.CurDay.Hotel2) { currentText.text += "<br>Hotel 2 Avialiable"; }
             if (managerTime.CurDay.Hotel3) { currentText.text += "<br>Hotel 3 Avialiable"; }
-            if (managerTime.CurDay.Hotel4) { currentText.text += "<br>Hotel 4 Avialiable"; }
-            else
+            if (managerTime.CurDay.Hotel4) { currentText.text += "<br>Hotel 4 Avialiable"; }            
+        }
+        else
             {
                 currentText.text += "<br>No Hotels Today";
             }
-        }
     }
 
     // Update is called once per frame
