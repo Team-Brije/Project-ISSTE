5fbee3ebd1d4b3290b87d9d72ba91d0633729f17
Shika <shikatastrophe@gmail.com>
2024-11-20 00:38:16 -0600

    Ya hay audio

---
+++ Assets/Scripts/AudioGetter.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioGetter : MonoBehaviour
+{
+    AudioSource audiosource;
+    DataManager manager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = DataManager.Instance;
+        audiosource = GetComponent<AudioSource>();
+        Invoke(nameof(GetAudio), 0.05f);
+    }
+
+    public void GetAudio()
+    {
+        audiosource.clip = manager.voiceline;
+        audiosource.Play();
+    }
+}
